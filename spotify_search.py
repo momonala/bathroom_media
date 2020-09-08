3a4781ea314ac69cab33ff5f530359a25a5273bf
Mohit Nalavadi <mnalavadi@gmail.com>
2020-09-08 13:52:03 +0200

    improve song ID parsing/caching

--- spotify_search.py
+++ spotify_search.py
@@ -1,21 +1,23 @@
 import json
 import logging
+import re
+from functools import partial
+from string import punctuation
 from typing import Dict, List
 
 import requests
 
-from values import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from values import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, PLAYLIST_URI
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-PLAYLIST_URI = "59L2W0PlCrvkX7zgq9Bzyv"
-
 
 class SpotifySearch:
     def __init__(self):
         self.tracks_and_names = self._get_tracks_and_names()
+        logger.info(f"Found tracks: {self.tracks_and_names}")
 
     @staticmethod
     def _get_token() -> Dict:
@@ -29,7 +31,7 @@ class SpotifySearch:
         return response
 
     @staticmethod
-    def _get_tracks(token) -> Dict:
+    def _get_tracks(token: str) -> Dict:
         url = f"https://api.spotify.com/v1/playlists/{PLAYLIST_URI}/tracks"
         headers = {"Authorization": f"Bearer {token}"}
         logger.info(f"Making request: {(url, headers)}")
@@ -38,11 +40,12 @@ class SpotifySearch:
         logger.debug(f"Got response: {response}")
         return response
 
-    def _get_tracks_and_names(self) -> List[Dict]:
+    def _get_tracks_and_names(self) -> List[Dict[str, str]]:
         token_resp = self._get_token()
         token = token_resp["access_token"]
         tracks_resp = self._get_tracks(token)
+        parse = partial(re.sub, pattern=fr"[{punctuation}]", repl="")
         return [
-            {"track": x["track"]["name"], "artist": x["track"]["artists"][0]["name"]}
+            {"track": parse(string=x["track"]["name"]), "artist": parse(string=x["track"]["artists"][0]["name"]),}
             for x in tracks_resp["items"]
         ]
--- youtube_search.py
+++ youtube_search.py
@@ -1,9 +1,9 @@
-import json
 import logging
 import os
-from typing import Dict
-from glob import glob
 import re
+from glob import glob
+from typing import Dict
+
 import googleapiclient.discovery
 import youtube_dl
 
@@ -19,9 +19,7 @@ class YouTubeSearch:
         self.search_terms = search_terms
         self.search_terms_str = f"{search_terms['track']} {search_terms['artist']}"
         self.video = None
-        self.cached_songs = [
-            re.search(r"_(.*).mp3", x).groups()[0] for x in glob("media/*mp3")
-        ]
+        self.cached_songs = [re.search(r"_(.*).mp3", x).groups()[0] for x in glob("media/*mp3")]
         if "".join(self.search_terms_str.split()) in self.cached_songs:
             logger.info(f"Found cached {self.search_terms_str}")
         else:
@@ -34,18 +32,13 @@ class YouTubeSearch:
     def to_dict(self) -> Dict:
         return self.video
 
-    def to_json(self) -> str:
-        return json.dumps({"videos": self.video})
-
     def _search(self) -> Dict[str, str]:
-        return {"id": "0XlUsEfcKB0", "title": "test video"}
+        # return {"id": "0XlUsEfcKB0", "title": "test video"}
         youtube = googleapiclient.discovery.build(
             "youtube", "v3", developerKey=YOUTUBE_DATA_V3_API_KEY, cache_discovery=False
         )
 
-        request = youtube.search().list(
-            part="snippet", q=self.search_terms, type="video", maxResults=1,
-        )
+        request = youtube.search().list(part="snippet", q=self.search_terms, type="video", maxResults=1,)
         response = request.execute()
         video_id = response["items"][0]["id"]["videoId"]
         title = response["items"][0]["snippet"]["title"]
@@ -53,15 +46,14 @@ class YouTubeSearch:
         return {"id": video_id, "title": title}
 
     def _download_youtube_video(self) -> None:
-        mp3_output = (
-            f"media/{self.video['id']}_{self.search_terms_str.replace(' ', '')}.mp3"
-        )
+        mp3_output = f"media/{self.video['id']}_{self.search_terms_str.replace(' ', '')}.mp3"
         if os.path.exists(mp3_output):
             logger.info(f"File {mp3_output} already exists. Skipping.")
             return
 
         options = {
-            # todo youtube-dl issue, cant download only audio, getting codec issue
+            # todo youtube-dl issue, cant download only audio, getting codec
+            # issue
             "outtmpl": "%(id)s" + ".mp4",
             "format": "bestaudio/best",
         }
@@ -71,9 +63,7 @@ class YouTubeSearch:
             logger.info(f"Sucessfully downloaded video file {self.video}.")
         try:
             # use ffmpeg to conver the .mp4 file to a .mp3
-            os.system(
-                f'ffmpeg -y -i {self.video["id"]}.mp4 -f mp3 -ab 192000 -vn {mp3_output}'
-            )
+            os.system(f'ffmpeg -y -i {self.video["id"]}.mp4 -f mp3 -ab 192000 -vn {mp3_output}')
         except FileNotFoundError as e:
             logger.error(f"Conversion failed for {self.video['id']}.")
             raise e
