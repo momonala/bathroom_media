ac415f3748701a1fd0dded87d6bc9e67c5911475
momonala <mnalavadi@gmail.com>
2022-02-15 11:19:06 +0100

    remove songs no longer in playlist

--- download_songs.py
+++ download_songs.py
@@ -1,8 +1,10 @@
 import logging
+import os
+from glob import glob
 from multiprocessing import Pool, cpu_count
 
 from spotify_search import SpotifySearch
-from youtube_search import download_youtube_video_if_needed
+from youtube_search import download_youtube_video_if_needed, parse_search_terms
 
 POOL_SIZE = cpu_count()
 
@@ -12,6 +14,10 @@ logger = logging.getLogger(__name__)
 
 def _download_all_songs() -> None:
     s = SpotifySearch()
+    to_be_cached = [parse_search_terms(x)[1] for x in s.tracks_and_names]
+    existing_cache = glob("media/*mp3")
+    to_delete = set(existing_cache) - set(to_be_cached)
+    [os.remove(f) for f in to_delete]
 
     with Pool(POOL_SIZE) as pool:
         pool.map(download_youtube_video_if_needed, s.tracks_and_names)
