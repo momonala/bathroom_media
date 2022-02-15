48311243e7397718e925f61bb6022e937744ea53
momonala <mnalavadi@gmail.com>
2022-02-15 11:18:54 +0100

    check for new songs on button click

--- player.py
+++ player.py
@@ -1,10 +1,8 @@
 import glob
-import subprocess
-
 import logging
+import random
+import subprocess
 import time
-from itertools import cycle
-from random import shuffle
 
 import paho.mqtt.client as mqtt
 
@@ -17,16 +15,19 @@ MQTT_TOPIC = "home/bathroom_button"
 MQTT_PORT = 1883
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, _, __, rc):
     """The callback for when the client receives a CONNACK response from the server."""
     client.subscribe(MQTT_TOPIC)
     logger.info(f'Connected to MQTT: {MQTT_SERVER=} {MQTT_TOPIC=} {MQTT_PORT=} {rc=}')
 
 
-def on_message(client, userdata, msg):
-    """The callback for when a PUBLISH message is received from the server."""
+def on_message(_, __, msg):
+    """The callback for when a PUBLISH message is received from the server.
+    Cycle through .mp3 files in /media, skip song if event trigger.
+    """
     logger.info(f"Received message: {msg.topic=} {msg.payload=}")
-    _file = next(userdata['audio_files'])
+    audio_files = glob.glob("media/*mp3")
+    _file = random.choice(audio_files)
     subprocess.Popen(f"pkill mpv".split())  # kill the old player process
     time.sleep(.1)
     subprocess.Popen(f"mpv {_file}".split())  # start a new one
@@ -34,13 +35,8 @@ def on_message(client, userdata, msg):
 
 
 if __name__ == "__main__":
-    # cycle through .mp3 files in /media, skip song if event trigger. If no files in cwd, try full pi path
-    audio_files = glob.glob("media/*mp3")
-    shuffle(audio_files)
-    audio_files = cycle(audio_files)
-
     # setup MQTT client and subscribe to messages
-    mqtt_client = mqtt.Client(userdata={"audio_files": audio_files})
+    mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
     mqtt_client.connect(MQTT_SERVER, 1883)
